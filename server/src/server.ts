8701d800251a303529bb68725f30f9ed1f49d6d0
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2023-01-19 00:51:05 -0300

    criaçao de novas rotas, adiçao de algumas libs e é isso

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,14 @@
 import Fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
 import cors from '@fastify/cors'
+import { appRoutes } from './routes';
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
-
-app.get('/hello', async () => {
-  const habits = await prisma.habit.findMany()
-  return habits
-})
+app.register(appRoutes)
 
 app.listen({
   port: 3333,
+}).then(() => {
+  console.log('HTTP Server running!');
 })
-  .then(() => {
-    console.log('HTTP Server running!');
-  })
