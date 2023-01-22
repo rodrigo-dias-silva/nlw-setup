9951916752ed464660f5e23e461abd3198040b82
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2023-01-21 21:48:12 -0300

    atualizaçao nas funcionalidades e estilizaçao + criaçao HabilitList.tsx devidamente configurado e estilizado

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -44,19 +44,21 @@ export default function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map(date => {
-          const dayInSummary = summary.find(day => {
-            return dayjs(date).isSame(day.date, 'day')
+        {
+          summary.length > 0 && summaryDates.map(date => {
+            const dayInSummary = summary.find(day => {
+              return dayjs(date).isSame(day.date, 'day')
+            })
+            return (
+              <HabitDay
+                key={date.toString()}
+                date={date}
+                amount={dayInSummary?.amount}
+                defaultCompleted={dayInSummary?.completed}
+              />
+            )
           })
-          return (
-            <HabitDay
-              key={date.toString()}
-              date={date}
-              amount={dayInSummary?.amount}
-              completed={dayInSummary?.completed}
-            />
-          )
-        })}
+        }
 
         {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
           return (
